a1db5fd33bfc77c19789c01861999a0260d651bf
Alon Jacobson <2022625@crl-1311a-16.cpsd.us>
2018-03-09 13:03:41 -0500

    Better Cookie Wabble

--- Assets/scripts/Circle.cs
+++ Assets/scripts/Circle.cs
@@ -15,33 +15,61 @@ public class Circle : MonoBehaviour {
 
 	public bool wobbleOn; // TODO: wobble
 
+	public float sine;
+	public float envelope = 0f;
+
+	public float actualSize;
+
+	public float envelopeIncrement = 0.02f;
+
+	float sineStartingFramecount;
+
 	void OnMouseDown() {
 		gameStats.score += gameStats.pointsPerClick;
+		incrementEnvelope ();
 	}
 
 	void OnMouseUp() {
 		onCookieClick = false;
+		incrementEnvelope ();
 	}
 
-	void OnMouseOver() {
+	void OnMouseOver() { // while mouse on me
+		//		envelope += 1f;
 		if (Input.GetMouseButton (0)) {
 			if (onCookieClick)
 				sizeDesired = 0.52f;
 			else
 				sizeDesired = 0.48f;
 		} else {
-			sizeDesired = 0.52f;
+			sizeDesired = 0.55f;
 		}
 	}
 
-	void OnMouseExit() {
+	void OnMouseExit() { // once when mouse leaves me
+		incrementEnvelope();
 		if (Input.GetMouseButton (0))
 			onCookieClick = true;
 		sizeDesired = 0.5f;
 	}
 
+	void OnMouseEnter() { // once when mouse enters me
+		incrementEnvelope();
+	}
+
 	void Update() {
+		envelope = Mathf.Clamp(envelope - 0.067f * envelopeIncrement, 0f, envelopeIncrement);
+
+		sine = Mathf.Sin (Time.frameCount / 1f) * envelope;
+
 		size = sizeDesired * 0.6f + size * 0.4f;
-		transform.localScale = new Vector2 (size, size);
+
+		actualSize = size + sine;
+
+		transform.localScale = new Vector2 (actualSize, actualSize);
+	}
+
+	void incrementEnvelope() {
+		envelope = envelopeIncrement;
 	}
 }

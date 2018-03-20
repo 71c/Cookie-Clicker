f297ad746192e9b8b2deb88a948d2c9ac84c37bb
Alon <alon@Alons-MacBook-Air.local>
2018-03-20 15:50:47 -0400

    eehhhhh

---
+++ Assets/scripts/BuildingUpgrade.cs
@@ -0,0 +1,7 @@
+﻿[System.Serializable]
+public class BuildingUpgrade {
+	public string name;
+	public string quantityNeeded; // was int
+	public string basePrice; // was long
+	public string description;
+}
---
+++ Assets/scripts/UpgradeButton.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UpgradeButton : MonoBehaviour {
+
+	public BuildingUpgrade upgrade;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
